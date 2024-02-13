1148eabc4484ec0aa4a9c64ba22c883e381814eb
Henryikenna <ikennaunegbu10@gmail.com>
2024-02-13 23:11:39 +0100

    first-commit

---
+++ android/app/src/main/kotlin/com/example/socialscan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.socialscan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors{
+  static const Color mainPurple = Color(0xFF7F1F9A);
+  static const Color mainGray = Color(0xFFECECEC);
+}
