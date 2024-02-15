c4095bf73fce58c4076279585c388f4cf073cab4
Oc-ean <michealolatunbosun603@gmail.com>
2024-02-15 20:44:40 +0100

    added settings_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:socialscan/views/auth/screens/sign_up_screen.dart';
+import 'package:socialscan/views/settings/screens/settings_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +22,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF7F1F9A)),
         useMaterial3: false,
       ),
-      home: const SignUpScreen(),
+      home: const SettingScreen(),
+      // home: const SignUpScreen(),
     );
   }
 }
--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -1,2 +1,6 @@
 String imagePath = 'assets';
 String googleIcon = '$imagePath/google_icon.png';
+
+/// svg icons.....
+String iconPath = 'assets/svg_icons';
+String forwardIcon = '$iconPath/forward.svg';
